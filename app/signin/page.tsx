91578026bb46cd6098b95ca0093fd5a8cfb90ee0
soham2k06 <sohmmdev@gmail.com>
2023-12-08 22:39:36 +0530

    auth config file path changed

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,9 @@
 import { CredentialsForm } from "@/components";
+import { authConfig } from "@/utils/authConfig";
 
 import { Box } from "@mui/material";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authConfig } from "../api/auth/[...nextauth]/route";
 
 async function page() {
   const session = await getServerSession(authConfig);
